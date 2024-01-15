26d5ccd11c6cae2f4f308b68863a8aabdcd1ec67
moriken4320 <moriken.4320@gmail.com>
2024-01-15 12:32:57 +0900

    first commit

---
+++ src/test/kotlin/com/example/practicekotlinspringapp/PracticeKotlinSpringAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.practicekotlinspringapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeKotlinSpringAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
