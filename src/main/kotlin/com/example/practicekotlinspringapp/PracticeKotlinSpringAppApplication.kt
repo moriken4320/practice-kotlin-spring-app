9fb7b6e8e1810429f9a20e064bf8074f9708286a
moriken4320 <moriken.4320@gmail.com>
2024-01-16 13:56:25 +0900

    hello world

--- src/main/kotlin/com/example/practicekotlinspringapp/PracticeKotlinSpringAppApplication.kt
+++ src/main/kotlin/com/example/practicekotlinspringapp/PracticeKotlinSpringAppApplication.kt
@@ -1,11 +1,23 @@
 package com.example.practicekotlinspringapp
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class PracticeKotlinSpringAppApplication
+@RestController
+class PracticeKotlinSpringAppApplication {
+	@GetMapping("/hello")
+	fun hello(@RequestParam(value = "name", defaultValue = "World") name: String?): String {
+		return String.format("Hello %s!", name)
+	}
 
-fun main(args: Array<String>) {
-	runApplication<PracticeKotlinSpringAppApplication>(*args)
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			SpringApplication.run(PracticeKotlinSpringAppApplication::class.java, *args)
+		}
+	}
 }
